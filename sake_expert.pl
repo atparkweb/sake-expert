5ece016a8aa5a854382485c594e041f0d1178211
Andrew Park <atpark333@gmail.com>
2021-02-25 00:44:06 +0900

    Implement basic predicate logic rule with test data

--- sake_expert.pl
+++ sake_expert.pl
@@ -1,7 +1,16 @@
-:- module(sake_expert, []).
+product('Yuki No Boshi Junmai Daiginjo', Taste, Prefecture, Food, Temp) :-
+    Taste = light/dry,
+    Prefecture = akita,
+    (Food = sushi; Food = fish),
+    (Temp = cold; Temp = room),!.
 
-:- use_module(library(csv)).
-:- use_module(library(pio)).
-
-phrase_from_file(parse_csv(frame(Header, Rows)), './data/products.csv').
-row(Rows, []).
+suggest(Prod) :-
+  write('What flavor profile [light/dry | light/sweet | rich/dry | rich/sweet]?'),
+  read(T0),
+  write('What prefecture?: '),
+  read(P0),
+  write('What food [sushi | meat | cheese | fish | fruit]?: '),
+  read(F0),
+  write('What temperature [ice | cold | room | warm | hot]?: '),
+  read(T1),
+  product(Prod,T0,P0,F0,T1).
