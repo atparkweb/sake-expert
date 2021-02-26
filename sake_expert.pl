56b9ba1f0a9a85bcc19a8a5d9a1b8fd7c10cd732
Andrew Park <atpark333@gmail.com>
2021-02-26 21:45:19 +0900

    Add a few more products

--- sake_expert.pl
+++ sake_expert.pl
@@ -3,6 +3,16 @@ product('Yuki No Boshi Junmai Daiginjo', Taste, Prefecture, Food, Temp) :-
     Prefecture = akita,
     (Food = sushi; Food = fish),
     (Temp = cold; Temp = room),!.
+product('Yuki No Boshi Junmai Ginjo', Taste, Prefecture, Food, Temp) :-
+    Taste = light/dry,
+    Prefecture = akita,
+    (Food = meat; Food = cheese),
+    (Temp = cold; Temp = room; Temp = warm),!.
+product('Yuki No Boshi Junmai', Taste, Prefecture, Food, Temp) :-
+    Taste = rich/dry,
+    Prefecture = akita,
+    (Food = fruit; Food = cheese),
+    (Temp = cold; Temp = room),!.
 
 suggest(Prod) :-
   write('What flavor profile [light/dry | light/sweet | rich/dry | rich/sweet]?'),
