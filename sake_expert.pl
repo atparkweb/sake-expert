04a105a1a47f9c04fb3f4cbcbc5278da44c3635b
Andrew Park <atpark333@gmail.com>
2021-09-02 17:13:48 +0900

    refactor: Clean up whitespace

--- sake_expert.pl
+++ sake_expert.pl
@@ -3,8 +3,8 @@
 :- use_module(mock_data).
 
 suggest(Prod) :-
-  write('What flavor profile? [light/dry | light/sweet | rich/dry | rich/sweet]'), read(T0),
+  write('What flavor profile? [ light/dry | light/sweet | rich/dry | rich/sweet ]: '), read(T0),
   write('What prefecture?: '), read(P0),
-  write('What food? [sushi | meat | cheese | fish | fruit]: '), read(F0),
-  write('What temperature? [ice | cold | room | warm | hot]: '), read(T1),
+  write('What food? [ sushi | meat | cheese | fish | fruit ]: '), read(F0),
+  write('What temperature? [ ice | cold | room | warm | hot ]: '), read(T1),
   product(Prod,T0,P0,F0,T1).
